78f69e9d8a74baef5baec52820c8ee719d575d0b
mayurr1698 <rmayur1698@gmail.com>
2026-06-27 15:27:40 +0530

    template for blog

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from snippets import posts
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
-#routes
 
-@app.get("/", response_class=HTMLResponse)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+#routes
+#template  response
+@app.get("/")
+@app.get("/home")
+def home(request: Request):
+    return templates.TemplateResponse(
+            request,
+            "home.html",
+            {"posts": posts, "title": "Home"},
+        )
+    
+#html response
 @app.get("/posts", response_class=HTMLResponse,include_in_schema=False)# will exclude from api doc
 def home():
     return f'<h1>{posts[0]['title']}</h1>'
