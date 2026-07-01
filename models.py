e7f94f7e5e69df7dad1cee4f8f744b275fc486a0
mayurr1698 <rmayur1698@gmail.com>
2026-07-01 23:08:55 +0530

    routers

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from datetime import UTC, datetime
 from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from database import Base
+from databaseAsync import Base
 
 
 class User(Base):
