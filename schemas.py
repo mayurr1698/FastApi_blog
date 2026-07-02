29928440b92d5fdb9cd19cee26c6a24895ee7820
mayurr1698 <rmayur1698@gmail.com>
2026-07-02 09:39:07 +0530

    ui changes + auth

--- schemas.py
+++ schemas.py
@@ -6,19 +6,27 @@ class UserBase(BaseModel):
     email: EmailStr = Field(max_length=120)
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
-class UserResponse(UserBase):
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
+    username: str
     image_file: str | None
     image_path: str
+    
+class UserPrivate(UserPublic):
+    email: EmailStr
     
 class PostBase(BaseModel):
     title: str = Field(min_length=1,max_length=100)
@@ -39,5 +47,5 @@ class PostResponse(PostBase):
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
     
